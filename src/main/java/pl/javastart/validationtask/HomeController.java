0002fd50852b5fd4f7075586b7d6135d9956e6b5
Marcin Kunert <the.martines@gmail.com>
2021-03-03 10:27:07 +0100

    Przygotowanie zadania

---
+++ src/main/java/pl/javastart/validationtask/HomeController.java
@@ -0,0 +1,24 @@
+package pl.javastart.validationtask;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("data", new RegisterFormDto());
+        return "home";
+    }
+
+    @PostMapping("/register")
+    public String register(RegisterFormDto registerFormDto, Model model) {
+        model.addAttribute("formData", registerFormDto);
+        return "success";
+    }
+
+
+}
---
+++ src/main/java/pl/javastart/validationtask/RegisterFormDto.java
@@ -0,0 +1,77 @@
+package pl.javastart.validationtask;
+
+public class RegisterFormDto {
+
+    private String firstName;
+    private String surname;
+    private String address;
+    private String postalCode;
+    private String city;
+    private String email;
+    private String password;
+    private boolean termsAgreement;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isTermsAgreement() {
+        return termsAgreement;
+    }
+
+    public void setTermsAgreement(boolean termsAgreement) {
+        this.termsAgreement = termsAgreement;
+    }
+}
